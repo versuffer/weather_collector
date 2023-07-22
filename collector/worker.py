2c24a1d5b73b137f966a58acd504aab9c9403c35
versuffer <versuffer@mail.ru>
2023-07-22 12:12:49 +0300

    Periodic tasks launched

--- collector/worker.py
+++ collector/worker.py
@@ -5,11 +5,16 @@ import time
 
 celery_app = Celery(__name__)
 celery_app.conf.broker_url = os.environ.get("CELERY_BROKER_URL")
+celery_app.conf.beat_schedule = {
+    'print_hello': {
+        'task': 'print_hello',
+        'schedule': 2.0,
+    },
+}
+celery_app.conf.timezone = 'UTC'
 
 
 @celery_app.task(name="print_hello", ignore_result=True)
 def print_hello():
     print("hello")
-    time.sleep(60)
-    print("world")
     return True
